64dfe6e5642dda09177fc54f93b048817e9b4c70
Jackie Liu <jackie.anj.liu@gmail.com>
2019-07-11 21:11:56 -0400

    updated content script to only run when icon is clicked

--- content.js
+++ content.js
@@ -1,8 +1,15 @@
 console.log("Chrome Extension Ready");
 
-var text = document.getElementsByTagName("p");
+chrome.runtime.onMessage.addListener(gotMessage);
 
-// loop through all the text to find times
-for (elt of text) {
-    elt.style['background-color'] = "red";
+function gotMessage(message, sender, sendResponse) {
+    console.log(message.txt);
+    if (message.txt === "hello") {
+        let text = document.getElementsByTagName('p');
+
+        // loop through all the text to find times
+        for (elt of text) {
+            elt.style['background-color'] = 'red';
+        }
+    }
 }
